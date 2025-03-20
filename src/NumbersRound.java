c662b2ae2c03409658413eb503e8c1c9dd742609
Kaan Taşcı <k_mert@windowslive.com>
2025-03-21 01:24:29 +0300

    Proje Tamamlandı.

--- src/NumbersRound.java
+++ src/NumbersRound.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class NumbersRound {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //kullanıcıya girdi istedim.
+        System.out.println("Sayıyı Giriniz: ");
+        double sayi = input.nextDouble();
+
+        //int yapmamdaki sebep istenilen çıktı ondalıklı değil.
+        int asagiYuvarlama = (int) Math.floor(sayi);
+        int yukariYuvarlama = (int) Math.ceil(sayi);
+        int enYakinTamSayi = (int) Math.round(sayi);
+
+        // Sonuçları ekrana yazdırdım.
+        System.out.println("Aşağı Yuvarlama: " + asagiYuvarlama);
+        System.out.println("Yukarı Yuvarlama: " + yukariYuvarlama);
+        System.out.println("En Yakın Tam Sayı: " + enYakinTamSayi);
+    }
+
 }
---
+++ src/project.md
@@ -0,0 +1,12 @@
+Sayı Yuvarlama (OPSİYONEL)
+Kullanıcının girdiği ondalıklı bir sayıyı aşağı, yukarı ve en yakın tam sayıya yuvarlayarak ekrana yazdıran kodu yazınız.
+
+Input: 5.7
+
+Output:
+
+Aşağı Yuvarlama: 5
+
+Yukarı Yuvarlama: 6
+
+En Yakın Tam Sayı: 6
